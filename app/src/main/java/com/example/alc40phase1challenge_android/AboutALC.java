2478e259d344c54a8ea50e68cd1132d7a84eeaf9
ginowine <emailosahon@yahoo.com>
2019-07-12 14:22:08 +0100

    about us page now works

--- app/src/main/java/com/example/alc40phase1challenge_android/AboutALC.java
+++ app/src/main/java/com/example/alc40phase1challenge_android/AboutALC.java
@@ -1,13 +1,19 @@
 package com.example.alc40phase1challenge_android;
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutALC extends AppCompatActivity {
+
+    WebView aboutUsWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +22,41 @@ public class AboutALC extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        aboutUsWebView = (WebView) findViewById(R.id.aboutus_webview);
+
+        if (savedInstanceState != null){
+            aboutUsWebView.restoreState(savedInstanceState);
+            aboutUsWebView.getSettings().setJavaScriptEnabled(true);
+            webClient();
+        }else {
+            aboutUsWebView.getSettings().setJavaScriptEnabled(true);
+            webClient();
+            aboutUsWebView.loadUrl("https://andela.com/alc");
+        }
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        aboutUsWebView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    public void webClient(){
+        aboutUsWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
             }
         });
     }
